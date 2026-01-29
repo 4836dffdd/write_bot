c3e02dae05bff277dcc4413e47062a683d9a2faa
Chen <chen@gmail.com>
2026-01-29 13:14:00 +0800

    新变量

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hello")
+import os
+from dotenv import load_dotenv
+
+# 加载 .env 文件到系统环境变量
+load_dotenv()
+
+# 读取变量
+api_key = os.getenv("SILICONFLOW_API_KEY")
+base_url = os.getenv("SILICONFLOW_BASE_URL")
